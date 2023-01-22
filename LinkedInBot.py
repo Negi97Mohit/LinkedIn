e3873ad20a05ad8f95a0aedec64688e902def680
Negi97Mohit <mohit.snegi97@gmail.com>
2023-01-21 22:16:16 -0500

    Updated the file

--- LinkedInBot.py
+++ LinkedInBot.py
@@ -1,4 +1,3 @@
-
 #importing libraries
 from lib2to3.pgen2 import driver
 import time
@@ -6,11 +5,11 @@ from click import option
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.edge.service import Service
-from selenium.webdriver.edge.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
-
+from selenium.common.exceptions import WebDriverException
 
 #Main Bot class
 class LinkedIn:
@@ -20,28 +19,35 @@ class LinkedIn:
     def __init__(self,link):
         self.link=link
         service=Service()
-        driver = webdriver.Edge(service=service)   #connecting to me Edge browse
+        options=Options()
+        options.binary_location=r'C:\Program Files\Google\Chrome\Application\chrome.exe'
+        driver = webdriver.Chrome(options=options)
+        driver = webdriver.Chrome(service=service)   #connecting to me Edge browse
         driver.get(self.link)     #connecting to the LinkedIn login page
 
     #This function will login using my credentials into my linkedIn account
     def login(self):
         service=Service()
-        self.driver = webdriver.Edge(service=service)   #connecting to me Edge browse
+        self.driver = webdriver.Chrome(service=service)   #connecting to me Edge browse
         self.driver.get(self.link)     #connecting to the LinkedIn login page
         print(self.link)
         print(self.driver.title)
 
         #my login credentials
-        username=#email
-        password=#password
+        username="mohit.snegi97@gmail.com"
+        password="Hollyhalston97)"
 
         #logining In
-        self.driver.find_element_by_id("username").send_keys(username)
-        self.driver.find_element_by_id("password").send_keys(password)
-        self.driver.find_element_by_class_name("login__form_action_container ").click()
-                
+        uname_xpath="//input[@id='username']"
+        pass_xpath="//input[@id='password']"
+        self.driver.find_element(By.XPATH, uname_xpath).send_keys(username)
+        self.driver.find_element(By.XPATH,pass_xpath).send_keys(password)
+        # self.driver.find_element_by_id("password").send_keys(password)
+        self.driver.find_element(By.XPATH,"//button[@class='btn__primary--large from__button--floating']").click()
+
+
         #redirecting to Cengage website
-        self.driver.get("https://www.linkedin.com/company/insulet-corporation/people/?facetCurrentFunction=12&facetGeoRegion=us%3A7&facetSkillExplicit=993%2C49")
+        self.driver.get("https://www.linkedin.com/company/state-street/people/?facetGeoRegion=90000007")
         time.sleep(15)
         get_title=self.driver.title
         
